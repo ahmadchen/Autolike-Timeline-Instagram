b5f987f6b5f21f1a7fbe29076da8be913013098c
egac <40325661+egac@users.noreply.github.com>
2020-11-12 15:16:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 /* 
  * Pace Usa Offical Bot © 2013 Made This
  *
- * developerpaceusa@gmail.com - 
+ * egacihuy@gmail.com - 
  *
  */
 @ini_set('memory_limit', '-1');
@@ -20,8 +20,8 @@ $Pace=array(
 		/*
 			SILAHKAN CING DI MASUKIN PELAN PELAN :V
 		*/
-		'', // USERNAME instagram lu
-		'', // PASSWORD instagram lu
+		'', // egacihuy@gmail.com
+		'', //
 		/* JANGAN DI EDIT.. !!! */
 		'https://i.instagram.com/api/v1/', 'Instagram 9.0.1 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)', '96724bcbd4fb3e608074e185f2d4f119156fcca061692a4a1db1c7bf142d3e22'
 	),
